5c7c309b06acce63b967bc99cd7acf34f83cddf0
Mohamed Habib <moe.habib9@gmail.com>
2023-07-27 20:03:20 +0300

    refactor changes

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "digger-terraform-state-209539466991"
-    region         = "us-east-1"
-    dynamodb_table = "digger-terraform-state-lock"
-    key            = "drift-detection-demo-state"
-  }
-}
